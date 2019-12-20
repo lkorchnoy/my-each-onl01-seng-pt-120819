401f7b14cc9f10061f3882e1189d4f274f4fd9b1
Ludmila Korchnoy <lkorchnoy@yahoo.com>
2019-12-20 15:13:01 +0000

    Done.

--- my_each.rb
+++ my_each.rb
@@ -1,3 +1,31 @@
-def my_each # put argument(s) here
-  # code here
+def my_each(words)
+  i = 0
+  while i < words.length
+  words.each do |word|
+     puts "#{word}"
+   i = i + 1
+  end
 end
+words
+end
+
+  def my_each(tas)
+    i = 0
+    new_tas = []
+    while i < tas.length
+    tas.each do |ta|
+    new_tas << yield(tas[i])
+    i = i + 1
+  end
+end
+new_tas
+end
+  
+  
+  
+  
+  
+  
+  
+ 
+  
